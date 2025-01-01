feaceaf8189a842eaad4d54fc73b6db32c9d8434
Safirenat <saunakva@yandex.ru>
2025-01-01 23:25:24 +0300

    Создание структуры

---
+++ src/app/components/ui/header/index.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { FC, useEffect, useState } from "react";
+import Link from "next/link";
+import s from "./header.module.scss";
+
+const Header: FC = () => {
+
+  return (
+
+    <>
+    Header
+    </>
+
+  );
+
+};
+
+export default Header;
