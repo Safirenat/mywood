192a027d171863cd1f48c2f76328ff999976044e
Safirenat <saunakva@yandex.ru>
2025-01-02 12:39:16 +0300

    Вёрстка хедера

--- src/app/components/ui/header/index.tsx
+++ src/app/components/ui/header/index.tsx
@@ -1,15 +1,70 @@
 'use client'
 
-import { FC, useEffect, useState } from "react";
-import Link from "next/link";
+import { FC } from "react";
 import s from "./header.module.scss";
+import Logo from "../logo";
+import PhoneIcon from "../../icons/phone";
+import LikeIcon from "../../icons/like";
+import BasketIcon from "../../icons/basket";
 
 const Header: FC = () => {
 
   return (
 
     <>
-    Header
+
+      <div className = { s.root }>
+
+        <div className = { s.left }>
+
+          <div className = { s.logo }><Logo/></div>
+
+          <div className = { s.navWrap }>
+
+            <div className = { s.phoneWrap }>
+
+              <PhoneIcon/>
+
+              <div className = { s.phone }>
+              
+                +7919646-50-74
+
+              </div>
+
+            </div>
+
+            <div className = { s.nav }>
+
+              <div>Каталог</div>
+              <div>Наши работы</div>
+              <div>О компании</div>
+
+            </div>
+
+          </div>
+
+        </div>
+        
+        <div className = { s.right }>
+
+          <input placeholder = "Найти на сайте"/>
+
+          <div>
+
+            <LikeIcon/>
+
+          </div>
+
+          <div>
+
+            <BasketIcon/>
+
+          </div>
+        
+        </div>
+      
+      </div>
+
     </>
 
   );
