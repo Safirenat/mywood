0259add9962fe14d740596d9c664f0c71ce045fa
Safirenat <saunakva@yandex.ru>
2025-01-02 21:08:47 +0300

    Верстка секции Home

---
+++ src/app/components/sections/home.section/index.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { FC } from "react";
+import s from "./home.section.module.scss";
+import LogoBigIcon from "../../icons/logo-big";
+import ProductionIcon from "../../icons/production";
+import DrawingsIcon from "../../icons/drawings";
+import DeliveryIcon from "../../icons/delivery";
+import GuaranteeIcon from "../../icons/guarantee";
+import CheckMarkIcon from "../../icons/check-mark";
+
+const HomeSection: FC = () => {
+
+  return (
+
+    <div className = { s.root }>
+
+      <div className = { s.title }>Качественный<br/> <span>изделия из дерева</span><br/> от производителя</div>
+      <div className = { s.subtitle }>Предоставляем инновационные, качественные<br/> и стильные решения для бань и саун.</div>
+
+      <div className = { s.icons }>
+
+        <div className = { s.item }>
+
+          <ProductionIcon/>
+          <div>Собственное производство</div>
+
+        </div>
+
+        <div className = { s.item }>
+
+          <DrawingsIcon/>
+          <div>Уникальные чертежи</div>
+
+        </div>
+
+        <div className = { s.item }>
+
+          <DeliveryIcon/>
+          <div>Доставка по всей России</div>
+
+        </div>
+
+        <div className = { s.item }>
+
+          <GuaranteeIcon/>
+          <div>Гарантия 12 месяцев</div>
+
+        </div>
+
+        <div className = { s.item }>
+
+          <CheckMarkIcon/>
+          <div>Заказ от еденицы товара</div>
+
+        </div>
+
+
+      </div>
+
+      <div className = { s.img }><LogoBigIcon/></div>
+
+    </div>
+
+  );
+
+};
+
+export default HomeSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import MainContainer from "./components/containers/main.container";
+import HomeSection from "./components/sections/home.section";
 
 export default function Home() {
 
@@ -10,12 +11,7 @@ export default function Home() {
 
     <MainContainer>
 
-    <div>
-      Block
-      Block
-      Block
-      Block
-    </div>
+      <HomeSection/>
 
     </MainContainer>
 
