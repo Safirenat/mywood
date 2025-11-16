4674cfccd4b6eeefc2605ecab95a32d65e38a945
Safirenat <saunakva@yandex.ru>
2025-11-16 22:28:51 +0300

    Обнуление

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['avatars.mds.yandex.net'],
-    unoptimized: true,
-  },
-  output: 'export',
-  trailingSlash: true, // ← ОБЯЗАТЕЛЬНО для экспортируемых кириллических маршрутов!
+  /* config options here */
 };
 
 export default nextConfig;
-
