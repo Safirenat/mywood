87080623c241feb50b2d490d9168b2e3c6ce54a7
Safirenat <saunakva@yandex.ru>
2025-11-17 00:53:46 +0300

    Каркас категорий(кружочки)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Container from "../components/ui/Container/Container";
 import HeroSlider from "../components/sections/HeroSlider/HeroSlider";
+import CategoryCircles from "../components/sections/CategoryCircles/CategoryCircles";
 
 export default function HomePage() {
   return (
-    // <Container>
-      <HeroSlider />
-    // </Container>
+    <>
+      {/* <Container> */}
+        <HeroSlider />
+        <CategoryCircles />
+      {/* </Container> */}
+    </>
   );
 }
---
+++ src/components/sections/CategoryCircles/CategoryCircles.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import styles from "./CategoryCircles.module.scss";
+
+const categories = [
+  {
+    id: 1,
+    title: "деревянные двери",
+    image: "/CategoryCircles/wood-door.svg",
+  },
+  {
+    id: 2,
+    title: "стеклянные двери",
+    image: "/CategoryCircles/glass-door.svg",
+  },
+  {
+    id: 3,
+    title: "форточки",
+    image: "/CategoryCircles/window.svg",
+  },
+  {
+    id: 4,
+    title: "шезлонги",
+    image: "/CategoryCircles/chair.svg",
+  },
+  {
+    id: 5,
+    title: "подголовники",
+    image: "/CategoryCircles/headrest.svg",
+  },
+  {
+    id: 6,
+    title: "прочее",
+    image: "/CategoryCircles/other.svg",
+  },
+  {
+    id: 7,
+    title: "скидки",
+    image: "/sale.png",
+  },
+];
+
+export default function CategoryCircles() {
+  return (
+    <section className={styles.Categories}>
+      <div className={styles.Inner}>
+        {categories.map((cat) => (
+          <div key={cat.id} className={styles.Item}>
+            <div className={styles.Circle}>
+              <Image
+                src={cat.image}
+                alt={cat.title}
+                width={80}
+                height={80}
+                quality={90}
+              />
+            </div>
+            <p className={styles.Label}>{cat.title}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
